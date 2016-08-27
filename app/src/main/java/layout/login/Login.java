91b342c6eed26c3b79120496f36cc1fcbfe7af0e
FdeFabricio <fabriciocomf@gmail.com>
2016-08-27 18:13:56 -0300

    Tela Home

---
+++ app/src/main/java/layout/login/Login.java
@@ -0,0 +1,20 @@
+package layout.login;
+
+import android.graphics.Color;
+import android.graphics.PorterDuff;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.EditText;
+
+import com.dcc.hackathon.locus.R;
+
+public class Login extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+
+    }
+}
