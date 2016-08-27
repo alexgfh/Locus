d4581abef1d7f7a25621b655a39b444ce52d199c
alexgfh <alexgfh@gmail.com>
2016-08-27 18:27:10 -0300

    Passa da tela de login para tela do mapa

--- app/src/main/java/layout/login/Login.java
+++ app/src/main/java/layout/login/Login.java
@@ -1,11 +1,14 @@
 package layout.login;
 
+import android.content.Intent;
 import android.graphics.Color;
 import android.graphics.PorterDuff;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.EditText;
 
+import com.dcc.hackathon.locus.MapsActivity;
 import com.dcc.hackathon.locus.R;
 
 public class Login extends AppCompatActivity {
@@ -15,6 +18,9 @@ public class Login extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
+    }
 
+    public void login(View v) {
+        startActivity(new Intent(this, MapsActivity.class));
     }
 }
